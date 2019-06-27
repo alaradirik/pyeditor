a706a845b4eac091d71858c0f8bb205309eb5b36
dirik <alara@pactly.org>
2019-06-27 17:56:15 +0300

    display uploaded image

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask, render_template, request
-
 import os
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
-app.config['UPLOADED_PATH'] = os.path.join(app.root_path, 'images/uploads')
 
-### TODO: mkdir -> images/uploads and images/processed
-### TODO: process images -> crop and correct for skew, brightness
+app = Flask(__name__, static_url_path='/static')
+UPLOAD_FOLDER = os.path.join(app.root_path, 'static/images/uploads')
+print(UPLOAD_FOLDER)
+
+### TODO: process uploaded images -> crop and correct for skew, brightness
+### TODO: extract text from processed images
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
         for file in request.files.getlist('file'):
-            file.save(os.path.join(app.config['UPLOADED_PATH'], file.filename))
+            filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+            if not os.path.exists(filepath):
+                os.makedirs(filepath)
+            file.save(filepath)
+
+        return "OK"
+
     return render_template('index.html')
+
+@app.route('/editor')
+def serve_processed_image():
+    valid_extensions = ['jpg','jpeg', 'bmp', 'png', 'gif']
+    filename = [fn for fn in os.listdir(UPLOAD_FOLDER)
+              if any(fn.endswith(ext) for ext in valid_extensions)][0]
+    filepath = os.path.join('/static/images/uploads', filename)
+
+    return render_template('editor.html', filepath=filepath)
 
 
 if __name__ == "__main__":
